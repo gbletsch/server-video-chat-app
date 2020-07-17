5ffa191f13ffa1d6dd4fb06a11b5c6dde437d6da
Guilherme <gbletsch@gmail.com>
2020-07-17 09:29:07 -0300

    server ok

--- server.js
+++ server.js
@@ -5,7 +5,33 @@ const server = http.createServer(app)
 const socket = require('socket.io')
 const io = socket(server)
 
+const rooms = {} // object of lists
 
+io.on('connection', socket => {
+  socket('join room', roomID => {
+    if (rooms[roomID]) {
+      rooms[roomID].push(socket.id)
+    } else {
+      rooms[roomID] = [socket.id]
+    }
+    const otherUser = rooms[roomID].find(id => id !== socket.id) // otherUser = id
+    if (otherUser) {
+      socket.emit('other user', otherUser)
+      socket.to(otherUser).emit('user joined', socket.id)
+    }
+  })
+
+  socket.on('offer', payload => {
+    io.to(payload.target).emit('offer', payload)
+  })
+  socket.on('answer', payload => {
+    io.to(payload.target).emit('answer', payload)
+  })
+  socket.on('ice-candidate', incoming => {
+    io.to(incoming.target).emit('ice-candidate', incoming)
+  })
+
+})
 
 const port = process.env.PORT || 8000
 server.listen(port, () => console.log(`server is running on port ${port}`))
